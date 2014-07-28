5a598f963734da853ca0360d6388893b996506b7
Sayanti Banerjee <sayanti@sayantis-mbp.gateway.2wire.net>
2014-07-27 18:34:55 -0700

    Submitting programming assignment 2 (cachematrix.R): Sayanti Banerjee

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following pair of functions cache the inverse of a given matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special "matrix" that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL ##initialize the inverse
+        
+        ## set the values of the matrix
+        set <- function(y = matrix()){  
+                x <<- y 
+                i <<- NULL
+                
+       }
+       
+       ## get the matrix
+       get <- function() x
+       
+       ## set the inverse of the matrix
+       setinv <- function(inverse) i <<- inverse
+       
+       ## get the inverse of the matrix
+       getinv <- function() i
+       
+       ## returns a special matrix object, which is a list containing the functions set, get, setinv, and getinv
+       list(set = set, get = get,
+            setinv = setinv,
+            getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cacheSolve function
+## retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+        i <- x$getinv() ## get the inverse of the matrix "x"
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i) ## if the inverse has already been calculated, return the inverse from the cache 
+        }
+        
+        mat <- x$get() ## get the matrix from the cache
+        i <- solve(mat) ## calculate the inverse 
+        x$setinv(i) ## set the inverse in the cache
+        
+        i  ## return the inverse
+        
+        
 }
